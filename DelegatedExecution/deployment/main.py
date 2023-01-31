cd112b9d821623c3efd7cf66be0f0bddf5cc4b9d
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-01-31 09:37:04 -0300

    updated gitignore

---
+++ DelegatedExecution/deployment/main.py
@@ -0,0 +1,4 @@
+
+class Contract:
+    pass
+
--- Zero Trust Rollups/Contracts/APIConsumer.sol
+++ Zero Trust Rollups/Contracts/APIConsumer.sol
@@ -7,9 +7,27 @@ import "./APIProvider.sol";
 contract APIConsumer {
     // TODO esto es un ejemplo de ZT rollup, o ZT Oracle
     APIProvider public provider;
-
+    //web3.personal.sign(hash, web3.eth.defaultAccount, console.log)
     constructor(address apiProviderAddress) {
         provider = APIProvider(apiProviderAddress);
+    }
+
+    function verifySignature(
+        bytes32 signedHash, bytes calldata signature, address signer
+    ) public pure returns (bool) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
+        return (ecrecover(signedHash, v, r, s) == signer);
+    }
+
+    function splitSignature(
+        bytes memory signature
+    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(signature.length == 65, "invalid signature length");
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+            v := byte(0, mload(add(signature, 96)))
+        }
     }
 
 }
