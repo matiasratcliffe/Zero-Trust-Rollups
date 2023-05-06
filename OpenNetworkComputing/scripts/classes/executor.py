024a6f80507f9c19f8091c014751b8050442339c
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-05-06 17:29:33 -0300

    more tests

--- OpenNetworkComputing/scripts/classes/executor.py
+++ OpenNetworkComputing/scripts/classes/executor.py
@@ -3,12 +3,23 @@
 
 class Executor:
 
-    def __init__(self, broker, account, register=False):
+    def __init__(self, broker, account, register=False, stake=None):
         self.broker = broker
         self.account = account
         if register:
-            self.broker.re
+            if stake == None:
+                stake = broker.BASE_STAKE_AMOUNT()
+            self.broker.registerExecutor({"from": self.account, "value": stake})
     
+    def getData(self):
+        return dict(self.broker.getExecutorByAddress(self.account))
+
+    def pauseExecutor(self, withdraw=False):
+        return self.broker.pauseExecutor(withdraw, {"from": self.account})
+    
+    def activateExecutor(self, funds=0):
+        pass
+
     def solveRound(self):
         if self.broker.getExecutorStateByAddress(self.account) == "locked":
             executor = dict(self.broker.getExecutorByAddress(self.account))
--- OpenNetworkComputing/tests/test_executor.py
+++ OpenNetworkComputing/tests/test_executor.py
@@ -2,7 +2,7 @@ from scripts.classes.utils.contractProvider import BrokerFactory
 from scripts.classes.utils.accountsManager import Accounts
 from scripts.classes.utils.logger import Logger
 from scripts.classes.executor import Executor
-
+import pytest
 
 class TestExecutor:
     def setup(self):
@@ -16,7 +16,6 @@ class TestExecutor:
 
     def teardown_method(self, method):
         pass
-        #network.event.event_watcher.reset()
 
     def test_creation(self):
         account = Accounts.getAccount()
@@ -26,43 +25,82 @@ class TestExecutor:
         assert BrokerFactory.at(address=executor.broker) == broker
     
     def test_create_and_register_new_executor_check_state(self):
-        pass
+        broker = BrokerFactory.getInstance()
+        executor = Executor(broker, Accounts.getAccount(), True)
+        assert broker.getExecutorStateByAddress(executor.account) == "active"
+        executorData = executor.getData()
+        assert executorData["accurateSolvings"] == 0
+        assert executorData["assignedRequestID"] == 0
+        assert executorData["inaccurateSolvings"] == 0
+        assert executorData["timesPunished"] == 0
+        assert executorData["executorAddress"] == executor.account
+        assert executorData["lockedWei"] == broker.BASE_STAKE_AMOUNT()
 
     def test_create_existing_executor_check_state(self):
-        pass
+        broker = BrokerFactory.getInstance()
+        account = Accounts.getAccount()
+        Executor(broker, account, True)
+        assert broker.getExecutorStateByAddress(account) == "active"
+        executor = Executor(broker, account, False)
+        assert broker.getExecutorStateByAddress(account) == "active"
 
     def test_create_and_register_existing_executor(self):
-        pass
-
-    def test_register_executor(self):
         broker = BrokerFactory.getInstance()
-        executor = Executor(broker, Accounts.getAccount())
-        pass
+        account = Accounts.getAccount()
+        Executor(broker, account, True)
+        assert broker.getExecutorStateByAddress(account) == "active"
+        with pytest.raises(Exception, match="This address is already registered as an active executor"):
+            Executor(broker, account, True)
 
     def test_resgiter_executor_without_enough_stake(self):
-        pass
-
-    def test_register_already_present_active_executor(self):
-        pass
+        with pytest.raises(Exception, match="To register an executor you must provide at least the minimum escrow stake amount"):
+            Executor(BrokerFactory.getInstance(), Accounts.getAccount(), True, 0)
 
     def test_pause_executor(self):
-        pass
+        executor = Executor(BrokerFactory.getInstance(), Accounts.getAccount(), True)
+        assert executor.broker.getExecutorStateByAddress(executor.account) == "active"
+        executor.pauseExecutor()
+        assert executor.broker.getExecutorStateByAddress(executor.account) == "inactive"
+        assert executor.getData()['lockedWei'] == executor.broker.BASE_STAKE_AMOUNT()
+    
+    def test_pause_paused_executor(self):
+        raise "implement this"
 
     def test_pause_executor_withdraw(self):
-        pass
+        executor = Executor(BrokerFactory.getInstance(), Accounts.getAccount(), True)
+        assert executor.broker.getExecutorStateByAddress(executor.account) == "active"
+        stake = executor.getData()["lockedWei"]
+        originalBalance = executor.account.balance()
+        transaction = executor.pauseExecutor(True)
+        assert executor.broker.getExecutorStateByAddress(executor.account) == "inactive"
+        assert executor.account.balance() == originalBalance - (transaction.gas_used * transaction.gas_price) + stake
+        assert executor.getData()['lockedWei'] == 0
 
     def test_register_already_present_inactive_executor(self):
-        pass
+        broker = BrokerFactory.getInstance()
+        account = Accounts.getAccount()
+        executor = Executor(broker, account, True)
+        executor.pauseExecutor()
+        assert executor.broker.getExecutorStateByAddress(executor.account) == "inactive"
+        with pytest.raises(Exception, match="The executor is already present, but inactive"):
+            Executor(broker, account, True)
 
     def test_activate_executor(self):
-        pass
+        broker = BrokerFactory.getInstance()
+        account = Accounts.getAccount()
+        executor = Executor(broker, account, True)
+        executor.pauseExecutor()
+        assert executor.broker.getExecutorStateByAddress(executor.account) == "inactive"
 
     def test_activate_non_paused_executor(self):
-        pass
+        raise "implement this"
 
     def test_activate_executor_without_enough_stake(self):
-        pass
+        raise "implement this"
+
+    def test_activate_executor_providing_stake(self):
+        raise "implement this"
 
     def test_register_already_present_busy_executor(self):
-        pass
+        raise "implement this"
 
