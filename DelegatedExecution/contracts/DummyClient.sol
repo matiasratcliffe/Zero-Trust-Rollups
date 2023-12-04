1814a4b2e9c1f0af455e3e0dc37e7a200ce6b5fe
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-12-04 18:39:48 -0300

    Added dummy client contract

---
+++ DelegatedExecution/contracts/DummyClient.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./BaseClient.sol";
+
+
+contract DummyClient is BaseClient {
+
+    constructor(address brokerAddress) BaseClient(brokerAddress) {}
+
+    function clientLogic(ClientInput calldata input) external override pure returns (bytes memory) {
+        bytes memory output = "";
+        return output;
+    }
+
+    function getInputStructure(uint functionID) external override pure returns (string memory) {
+        return "";
+    }
+}
